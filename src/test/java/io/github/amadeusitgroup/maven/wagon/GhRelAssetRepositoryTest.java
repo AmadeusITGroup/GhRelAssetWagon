2fb04ff7213b1c3e08547775f7238873ca36822c
scootafew <20837812+scootafew@users.noreply.github.com>
2025-09-29 14:03:43 +0100

    fix: consistently indent with 4 spaces

--- src/test/java/io/github/amadeusitgroup/maven/wagon/GhRelAssetRepositoryTest.java
+++ src/test/java/io/github/amadeusitgroup/maven/wagon/GhRelAssetRepositoryTest.java
@@ -8,32 +8,32 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class GhRelAssetRepositoryTest {
 
-  @ParameterizedTest
-  @CsvSource({
-      "ghrelasset://my-org/my-repo/v1.0.0/my-asset.zip, my-org/my-repo, v1.0.0, my-asset.zip",
-      "ghrelasset://another-org/another-repo/latest/another-asset.zip, another-org/another-repo, latest, another-asset.zip",
-      "ghrelasset://a/b/c/d.zip, a/b, c, d.zip"
-  })
-  void testValidRepositoryUrl(String url, String expectedRepo, String expectedTag, String expectedAsset) {
-    Repository repository = new Repository("someId", url);
-    GhRelAssetRepository ghRelAssetRepository = new GhRelAssetRepository(repository);
+    @ParameterizedTest
+    @CsvSource({
+            "ghrelasset://my-org/my-repo/v1.0.0/my-asset.zip, my-org/my-repo, v1.0.0, my-asset.zip",
+            "ghrelasset://another-org/another-repo/latest/another-asset.zip, another-org/another-repo, latest, another-asset.zip",
+            "ghrelasset://a/b/c/d.zip, a/b, c, d.zip"
+    })
+    void testValidRepositoryUrl(String url, String expectedRepo, String expectedTag, String expectedAsset) {
+        Repository repository = new Repository("someId", url);
+        GhRelAssetRepository ghRelAssetRepository = new GhRelAssetRepository(repository);
 
-    assertEquals(expectedRepo, ghRelAssetRepository.getGitHubRepository());
-    assertEquals(expectedTag, ghRelAssetRepository.getTag());
-    assertEquals(expectedAsset, ghRelAssetRepository.getAssetName());
-  }
+        assertEquals(expectedRepo, ghRelAssetRepository.getGitHubRepository());
+        assertEquals(expectedTag, ghRelAssetRepository.getTag());
+        assertEquals(expectedAsset, ghRelAssetRepository.getAssetName());
+    }
 
-  @ParameterizedTest
-  @ValueSource(strings = {
-      "ghrelasset://invalid-url",
-      "ghrelasset://org/repo/tag",
-      "ghrelasset://org/repo/tag/asset-without-zip-extension",
-      "ghrelasset://org/repo/tag/asset-ending-zip",
-      "http://github.com/org/repo",
-      ""
-  })
-  void testInvalidRepositoryUrl(String url) {
-    Repository repository = new Repository("someId", url);
-    assertThrows(IllegalArgumentException.class, () -> new GhRelAssetRepository(repository));
-  }
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "ghrelasset://invalid-url",
+            "ghrelasset://org/repo/tag",
+            "ghrelasset://org/repo/tag/asset-without-zip-extension",
+            "ghrelasset://org/repo/tag/asset-ending-zip",
+            "http://github.com/org/repo",
+            ""
+    })
+    void testInvalidRepositoryUrl(String url) {
+        Repository repository = new Repository("someId", url);
+        assertThrows(IllegalArgumentException.class, () -> new GhRelAssetRepository(repository));
+    }
 }
